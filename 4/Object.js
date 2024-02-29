aa81c6b958370601a2fed4286053740300eb83a8
Rahul Kumar <rahul790303@gmail.com>
2024-02-29 13:07:16 +0530

    Create Object.js

---
+++ 4/Object.js
@@ -0,0 +1,10 @@
+//in object we have the key pair value. it can be anything i.e can be int, string .
+
+let car = {
+  color: "black",
+  model: "2024",
+};
+console.log(car);
+console.log(car.color); //accessing the property of an object using  dot notation
+
+//we can create a empty object too
