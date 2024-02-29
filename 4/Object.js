c7b43bc21f3c09c45b803078c375b64b52e1b91d
Rahul Kumar <rahul790303@gmail.com>
2024-02-29 13:42:57 +0530

    Update Object.js

--- 4/Object.js
+++ 4/Object.js
@@ -3,8 +3,50 @@
 let car = {
   color: "black",
   model: "2024",
+  brand: "Maruti",
 };
 console.log(car);
 console.log(car.color); //accessing the property of an object using  dot notation
 
 //we can create a empty object too
+
+//Modify the Object Property
+
+car["color"] = "brown";
+console.log(car.color);
+
+delete car.model; //delete
+
+//Function Vs Method
+// Method - function declared inside object is called method
+// Function as 'Value'
+
+let person = {
+  ageCalculate: function (birthyear = 2008) {
+    let age = 2023 - birthyear;
+    return age;
+  },
+};
+
+console.log(`Current age is ${person.ageCalculate()}`);
+
+//this Keyword
+let person1 = {
+  firstName: "rahul",
+  lastName: "kumar",
+  getSummary: function () {
+    return `${this.firstName} ${this.lastName} output is from getSummary`;
+  },
+};
+
+console.log(person1.getSummary());
+
+//foreach method of Array
+
+//its another type of loop which we used to traverse over the array
+
+let dishes = ["biryani", "Channy", "Karahai", "Bhallay"];
+
+dishes.forEach(function (element) {
+  console.log(element);
+});
